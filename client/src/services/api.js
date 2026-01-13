03eb5f88268c028b3c3b118abe761a36e3ea6f4f
ESTHER <esther.kuria@student.moringaschool.com>
2026-01-13 10:35:58 +0300

    Dev learningproject

---
+++ client/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  console.log('API Request:', config.url, 'Token:', token ? 'Present' : 'Missing');
+  if (token) {
