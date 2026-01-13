b192f3bc009c3777f9be49903c192dde1a050c98
D142-msooh <kinyajuidama176@gmail.com>
2026-01-13 12:10:47 +0300

    Add new

--- client/src/pages/ApplyInstructor.js
+++ client/src/pages/ApplyInstructor.js
@@ -58,15 +58,7 @@ export default function ApplyInstructor({ user }) {
                   />
                 </div>
 
-                <div>
-                  <label className="block text-sm font-medium text-gray-700 mb-2">Email</label>
-                  <input
-                    type="email"
-                    value={user?.email}
-                    disabled
-                    className="w-full px-4 py-2 border border-gray-300 rounded-lg bg-gray-50 text-gray-600"
-                  />
-                </div>
+               
 
                 <div>
                   <label className="block text-sm font-medium text-gray-700 mb-2">
