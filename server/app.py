bf7783d165896b324afb59c0c086d517fdf3d7dd
+ <+>
2026-01-13 09:36:37 +0300

    message

---
+++ server/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_migrate import Migrate
+from config import Config
+from models import db, bcrypt, User, Course, Module, Enrollment, InstructorApplication
+from datetime import datetime
