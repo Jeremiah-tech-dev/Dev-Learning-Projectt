53aef4f6b8bd009aeeb8f1979056e784a35dee9d
ESTHER <esther.kuria@student.moringaschool.com>
2026-01-13 10:47:05 +0300

    dev learning project

--- client/src/services/App.js
+++ client/src/services/App.js
@@ -12,3 +12,22 @@ import ApplyInstructor from './pages/ApplyInstructor';
 import LessonViewer from './pages/LessonViewer';
 import { api } from './services/api';
 // import Footer from './components/Footer'; // TODO: create footer component
+
+export default function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      api.get('/auth/me')
+        .then(res => setUser(res.data))
+        .catch(() => {
+          localStorage.removeItem('token');
+          setUser(null);
+        })
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(false);
+    }
+  }, []);
