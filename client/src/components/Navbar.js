74fd6bc81bd415fc30f559c6b1fed45ba4435db2
D142-msooh <kinyajuidama176@gmail.com>
2026-01-13 10:50:50 +0300

    Add another new

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,3 +15,13 @@ const handleLogout = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center"></div>
+           <Link to="/courses" className="flex items-center space-x-3">
+              <div className="bg-blue-600 p-2">
+                <span className="text-white text-xl font-bold">DL</span>
+              </div>
+              <div>
+                <span className="text-xl font-bold text-white">
+                  DevLearn
+                </span>
+              </div>
+            </Link>
