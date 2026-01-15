0aca023cd3d9f0cdbc7d2692b303538ddd725a46
jeremiah-tech-dev <jeremiah.koske@student.moringaschool.com>
2026-01-15 14:42:20 +0300

    Fix duplicate imports in App.js

--- client/src/services/App.js
+++ client/src/services/App.js
@@ -12,17 +12,6 @@ import AdminDashboard from '../pages/AdminDashboard';
 import ApplyInstructor from '../pages/ApplyInstructor';
 import LessonViewer from '../pages/LessonViewer';
 import { api } from './api';
-import Navbar from './components/Navbar';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Courses from './pages/Courses';
-import CourseDetail from './pages/CourseDetail';
-import MyEnrollments from './pages/MyEnrollments';
-import InstructorDashboard from './pages/InstructorDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import ApplyInstructor from './pages/ApplyInstructor';
-import LessonViewer from './pages/LessonViewer';
-import { api } from './services/api';
 // import Footer from './components/Footer'; // TODO: create footer component
 
 export default function App() {
